ec015dc663087c7ffecc27994916e064508719ee
ziadasar <ziadasar@gmail.com>
2025-07-10 14:18:49 +0300

    added jquery for changing colors on click and hover effects

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  // Change color on click
+  $(".section, .header, .footer, .sidebar, .widget, .stats, .main").on("click", function () {
+    $(this).css("background-color", getRandomColor());
+  });
+
+  
+
+  // Highlight on hover
+  $(".section, .header, .footer, .sidebar, .widget, .stats, .main").hover(
+    function () {
+      $(this).addClass("active-hover");
+    },
+    function () {
+      $(this).removeClass("active-hover");
+    }
+  );
+
+ 
+
+  // Function to generate random colors
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+});
